5c2a536147ebd769ec9245f03ee8d540338fde25
123456@hzn.com <123456@hzn.com>
2023-03-22 11:06:26 +0800

    添加项目文件。

---
+++ scripts/main.js
@@ -0,0 +1,38 @@
+﻿let myHeading = document.querySelector('h1');
+alert('hello world!');
+document.querySelector("h1").addEventListener("click", ()=> {
+    myHeading.textContent = 'Hello world!';
+    alert("别戳我，我怕疼。");
+});
+
+
+let myImage = document.querySelector('img');
+myImage.onclick = function(){
+    let mySrc = myImage.getAttribute('src');
+    if (mySrc === 'images/firefox-icon.png') {
+        myImage.setAttribute('src', 'images/firefox2.png');
+    } else {
+        myImage.setAttribute('src', 'images/firefox-icon.png');
+    }
+}
+
+let myButton = document.querySelector('button');
+function setUserName() {
+    let myName = prompt('请输入你的名字。');
+    if (!myName) {
+        setUserName();
+    } else {
+        localStorage.setItem('name', myName);
+        myHeading.textContent = 'Mozilla 酷毙了，' + myName;
+    }
+}
+myButton.onclick = () => {
+    setUserName();
+};
+if (!localStorage.getItem('name')) {
+    setUserName();
+} else {
+    let storedName = localStorage.getItem('name');
+    myHeading.textContent = 'Mozilla 酷毙了，' + storedName;
+}
+
