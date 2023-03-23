f795c3b80c48ee1b22d0d7d72008c6e250300a7a
ZnunHwang <ZnunHwang@foxmail.com>
2023-03-23 11:36:14 +0800

    更改图片格式

--- scripts/main.js
+++ scripts/main.js
@@ -10,7 +10,7 @@ let myImage = document.querySelector('img');
 myImage.onclick = function(){
     let mySrc = myImage.getAttribute('src');
     if (mySrc === 'images/firefox-icon.png') {
-        myImage.setAttribute('src', 'images/firefox2.png');
+        myImage.setAttribute('src', 'images/firefox2.jpg');
     } else {
         myImage.setAttribute('src', 'images/firefox-icon.png');
     }
